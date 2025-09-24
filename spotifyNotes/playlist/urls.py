3a1ec1a1add7a96cef8fbe066d9d367ce0b543dd
CODECZERO <as8857981@gamil.com>
2025-09-24 11:57:55 +0530

    added whitenoise middleware

--- spotifyNotes/playlist/urls.py
+++ spotifyNotes/playlist/urls.py
@@ -5,6 +5,7 @@ app_name = 'playlist'
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', views.spotify_login, name='spotify_login'),
+    path('callback/', views.spotify_callback, name='spotify_callback'),
     path('track/<str:track_id>/notes/', views.notes_view, name='track_notes'),
     path('track/<str:track_id>/analysis/',
          views.track_notes_analysis, name='track_notes_analysis'),
